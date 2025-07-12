498f9fa3834d8da7452ca22121b337cd16755e16
Lyka <bin.lyka@institute.pse.ngo>
2025-07-12 09:17:34 +0700

    add

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,10 @@
 "use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { User, Mail, Lock } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -9,26 +15,18 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import z from "zod";
-import { useMutation } from "@tanstack/react-query";
-import { authRequest } from "@/lib/api/auth-api";
-import { AuthLoginForm } from "@/types/auth-type";
-import { useRouter } from "next/navigation";
 
 const LoginSchema = z.object({
-  user_name: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  user_name: z.string().min(3, {
+    message: "Username must be at least 3 characters.",
   }),
-  password: z.string().min(2, {
-    message: "Password must be at least 2 characters.",
+
+  password: z.string().min(4, {
+    message: "invalid password.",
   }),
 });
 
-const Login = () => {
-  const navigate = useRouter();
-  const { AUTH_LOGIN } = authRequest();
+const LoginPage = () => {
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -37,24 +35,18 @@ const Login = () => {
     },
   });
 
-  const { mutate, isPending } = useMutation({
-    mutationKey: ["login"],
-    mutationFn: (payload: AuthLoginForm) => AUTH_LOGIN(payload),
-    onSuccess: (data) => {
-      if (data) {
-        navigate.push("/");
-      }
-    },
-  });
+  function onSubmit(data: z.infer<typeof LoginSchema>) {
+    console.log(data);
+  }
 
-  const onSubmit = (data: z.infer<typeof LoginSchema>) => {
-    console.log(data, "===data login");
-    mutate(data);
-  };
   return (
-    <div className="mx-auto w-full max-w-md shadow-lg rounded-2xl p-4 mt-12">
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 bg-white rounded-3xl shadow-lg p-8 w-full max-w-md"
+        >
+          <h1 className="text-2xl font-bold">Login</h1>
           <FormField
             control={form.control}
             name="user_name"
@@ -62,7 +54,14 @@ const Login = () => {
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <Input placeholder="username" {...field} />
+                  <div className="relative">
+                    <User className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+                    <Input
+                      placeholder="username"
+                      {...field}
+                      className="pl-10"
+                    />
+                  </div>
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -75,19 +74,24 @@ const Login = () => {
               <FormItem>
                 <FormLabel>Password</FormLabel>
                 <FormControl>
-                  <Input type="password" placeholder="password" {...field} />
+                  <div className="relative">
+                    <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+                    <Input
+                      placeholder="password"
+                      {...field}
+                      className="pl-10"
+                    />
+                  </div>
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit" disabled={isPending}>
-            {isPending ? "Submitting" : "Submit"}
-          </Button>
+          <Button type="submit">Submit</Button>
         </form>
       </Form>
     </div>
   );
 };
 
-export default Login;
+export default LoginPage;
