f43692739eec9f81bdf74de4d2456188c8c5b8fe
Lyka <bin.lyka@institute.pse.ngo>
2025-07-24 07:55:15 +0700

    protect route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 "use client";
 
-import Profile from "./(dashboard)/profile/page";
-export default function Home() {
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import File from "./(dashboard)/profile/page";
+import { useAuthStore } from "@/store/auth-store";
+
+export default function Profile() {
+  const router = useRouter();
+
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.replace("/login");
+    }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated) return null;
+
   return (
     <div>
-      <Profile />
+      <File />
     </div>
   );
 }
